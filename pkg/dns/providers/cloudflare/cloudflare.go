879d772f94bc83459361089a85c89e2f94f5832f
Dave Conroy <dave@tiredofit.ca>
2025-05-23 15:56:15 -0700

    refine logging

--- pkg/dns/providers/cloudflare/cloudflare.go
+++ pkg/dns/providers/cloudflare/cloudflare.go
@@ -38,7 +38,7 @@ type Provider struct {
 // NewProvider creates a new Cloudflare DNS provider
 func NewProvider(config map[string]string) (dns.Provider, error) {
 	// Always use utils.GetProfileNameFromOptions for profile name resolution
-	profileName := utils.GetProfileNameFromOptions(config, "unknown")
+	profileName := utils.GetProfileNameFromOptions(config, "default")
 	logPrefix := fmt.Sprintf("[provider/cloudflare/%s]", profileName)
 
 	log.Trace("%s Resolved profile name: %s", logPrefix, profileName)
